68848f0391731185b8884616511ad5795e60ae80
Ido Ben-Hur <idoybh2@gmail.com>
2025-12-05 17:43:52 +0200

    Settings: Hide volume dialog position toggle for now

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -43,7 +43,6 @@ import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.sound.HandsFreeProfileOutputPreferenceController;
 import com.android.settings.sound.SliderVolumizer;
-import com.android.settings.sound.VolumeDialogPositionPreferenceController;
 import com.android.settings.sound.VolumeDialogTimeoutPreferenceController;
 import com.android.settings.sound.VolumeSliderPreference;
 import com.android.settings.widget.PreferenceCategoryController;
@@ -318,10 +317,7 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                 new VibrateIconPreferenceController(context, fragment, lifecycle);
         final VolumeDialogTimeoutPreferenceController volumeDialogTimeoutPreferenceController =
                 new VolumeDialogTimeoutPreferenceController(context);
-        final VolumeDialogPositionPreferenceController volumeDialogPositionPreferenceController =
-                new VolumeDialogPositionPreferenceController(context);
 
-        controllers.add(volumeDialogPositionPreferenceController);
         controllers.add(volumeDialogTimeoutPreferenceController);
         controllers.add(dialPadTonePreferenceController);
         controllers.add(inCallNotificationsPreferenceController);
