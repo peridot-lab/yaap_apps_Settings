f0eeed96144852d31aabb5b5fa5d18f61b725c8b
Ido Ben-Hur <idoybh2@gmail.com>
2025-12-22 17:46:11 +0200

    StatusBarSettings: Hide show battery percent
    AOSP now has this.

--- src/com/android/settings/system/StatusBarSettings.java
+++ src/com/android/settings/system/StatusBarSettings.java
@@ -59,12 +59,9 @@ public class StatusBarSettings extends DashboardFragment implements
     private static final String SYSTEMUI_PACKAGE = "com.android.systemui";
 
     private static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
-    private static final String SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
     private static final String CAMERA_MIC_INDICATOR_KEY = "camera_mic_icons_enabled";
 
     private SystemSettingMasterSwitchPreference mNetTrafficState;
-    private SystemSettingSwitchPreference mBatteryPercent;
-    private SystemSettingSwitchPreference mBatteryPercentInside;
     private SwitchPreferenceCompat mCameraMicIndicator;
 
     @Override
@@ -86,12 +83,6 @@ public class StatusBarSettings extends DashboardFragment implements
         mNetTrafficState.setChecked(enabled);
         updateNetTrafficSummary(enabled);
 
-        mBatteryPercent = findPreference(SHOW_BATTERY_PERCENT);
-        final boolean percentEnabled = Settings.System.getIntForUser(resolver,
-                SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT) == 1;
-        mBatteryPercent.setChecked(percentEnabled);
-        mBatteryPercent.setOnPreferenceChangeListener(this);
-
         mCameraMicIndicator = findPreference(CAMERA_MIC_INDICATOR_KEY);
         enabled = getDeviceConfig(CAMERA_MIC_INDICATOR_KEY);
         mCameraMicIndicator.setChecked(enabled);
@@ -112,11 +103,6 @@ public class StatusBarSettings extends DashboardFragment implements
             boolean enabled = (boolean) objValue;
             Settings.System.putInt(resolver, NETWORK_TRAFFIC_STATE, enabled ? 1 : 0);
             updateNetTrafficSummary(enabled);
-            return true;
-        } else if (preference == mBatteryPercent) {
-            boolean enabled = (boolean) objValue;
-            Settings.System.putInt(resolver,
-                    SHOW_BATTERY_PERCENT, enabled ? 1 : 0);
             return true;
         } else if (preference == mCameraMicIndicator) {
             boolean enabled = (boolean) objValue;
