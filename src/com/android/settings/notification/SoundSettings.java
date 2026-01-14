613fdfc489afd1d59a1d96a15a7d399a8a0a43fc
John Galt <johngaltfirstrun@gmail.com>
2026-01-14 10:49:22 -0500

    Revert "[WIP] Settings: Hide volume dialog position settings"
    This reverts commit f97dfa4d218167f96ed0aa2c643349fe14681353.

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -41,7 +41,7 @@ import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.sound.HandsFreeProfileOutputPreferenceController;
 import com.android.settings.sound.SliderVolumizer;
-// import com.android.settings.sound.VolumeDialogPositionPreferenceController;
+import com.android.settings.sound.VolumeDialogPositionPreferenceController;
 import com.android.settings.sound.VolumeDialogTimeoutPreferenceController;
 import com.android.settings.sound.VolumeSliderPreference;
 import com.android.settings.widget.PreferenceCategoryController;
@@ -294,10 +294,10 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                 new VibrateIconPreferenceController(context, fragment, lifecycle);
         final VolumeDialogTimeoutPreferenceController volumeDialogTimeoutPreferenceController =
                 new VolumeDialogTimeoutPreferenceController(context);
-        // final VolumeDialogPositionPreferenceController volumeDialogPositionPreferenceController =
-        //        new VolumeDialogPositionPreferenceController(context);
+        final VolumeDialogPositionPreferenceController volumeDialogPositionPreferenceController =
+                new VolumeDialogPositionPreferenceController(context);
 
-        // controllers.add(volumeDialogPositionPreferenceController);
+        controllers.add(volumeDialogPositionPreferenceController);
         controllers.add(volumeDialogTimeoutPreferenceController);
         controllers.add(dialPadTonePreferenceController);
         controllers.add(inCallNotificationsPreferenceController);
